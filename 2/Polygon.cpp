451fba4eeec9c96df42cde932a4422f5b0b30be8
Stefan Giggenbach <stefan.giggenbach@hm.edu>
2012-05-09 20:23:01 +0200

    modified doc for p1, created tarball p1 and modified p2

--- 2/Polygon.cpp
+++ 2/Polygon.cpp
@@ -1,11 +1,18 @@
 #include "Polygon.h"
 
 Polygon::Polygon() {
-	// TODO Auto-generated constructor stub
+  // TODO Auto-generated constructor stub
 
 }
 
 Polygon::~Polygon() {
-	// TODO Auto-generated destructor stub
+  // TODO Auto-generated destructor stub
 }
 
+float Polygon::area() const {
+  return 0;
+}
+
+bool Polygon::pip() const {
+  return true;
+}
--- 2/Polygon.h
+++ 2/Polygon.h
@@ -1,10 +1,27 @@
 #ifndef POLYGON_H_
 #define POLYGON_H_
 
+#include <vector>
+#include "Point2d.h"
+
+using namespace std;
+
 class Polygon {
+private:
+  vector<Point2d *> points;
 public:
-	Polygon();
-	virtual ~Polygon();
+  Polygon();
+  virtual ~Polygon();
+
+  /**
+   * calculate area of polynom
+   */
+  float area() const;
+
+  /**
+   * calculate area of polynom
+   */
+  bool pip() const;
 };
 
 #endif /* POLYGON_H_ */
