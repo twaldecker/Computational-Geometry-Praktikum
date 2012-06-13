6b922c58fcc0569bb5d5885982f92a0fe69d29b5
Thomas Waldecker <thomas.waldecker@googlemail.com>
2012-06-13 15:10:14 +0200

    intersection point calculation

--- 3/Line.cpp
+++ 3/Line.cpp
@@ -1,5 +1,6 @@
 #include "Line.h"
 
+
 Line::Line( const Point2d a, const Point2d b ) :
     a( a ), b( b ) {
 
@@ -16,8 +17,9 @@ Line::~Line() {
 
 /**
  * calculate if this line intersects with the provided Line l
+ * Point2d * intersection output of the intersection point
  */
-bool Line::intersect( const Line* l ) const {
+bool Line::intersect( const Line* l, Point2d * intersection ) const {
 
   const Line* p, *q;
   /* compare length of lines and assign pointers */
@@ -37,31 +39,23 @@ bool Line::intersect( const Line* l ) const {
 
   /* check collinearity of the two lines */
   if( fabs( ccwta ) <= FLT_EPSILON && fabs( ccwtb ) <= FLT_EPSILON ) { /* lines are collinear */
-
-    /* check for four identical points */
-    if( p->length() == 0 && q->length() == 0 )
-      return ( p->getA() == q->getA() );
-
-    /* now we test if the start or endpoint from q is in p */
-    Point2d lambda1 = ( q->getA() - p->getA() ) / ( p->getB() - p->getA() );
-    Point2d lambda2 = ( q->getB() - p->getA() ) / ( p->getB() - p->getA() );
-
-    /* we have to test both elements of lambda.
-     * Maybe we divided through zero if the line was horizontal or vertical.
-     * If one of the elements is between [0,1] of both lambdas we have a intersection.
-     */
-    if( ( lambda1.getX() >= 0 && lambda1.getX() <= 1 )
-        || ( lambda1.getY() >= 0 && lambda1.getY() <= 1 )
-        || ( lambda2.getX() >= 0 && lambda2.getX() <= 1 )
-        || ( lambda2.getY() >= 0 && lambda2.getY() <= 1 ) )
-      return true;
-    else
-      return false;
+    return false; /* if they are collinear, just return false. */
   }
 
   /* lines are not collinear -> check for intersection or(!) contact */
-  if( ( ( ccwta * ccwtb ) <= FLT_EPSILON )
-      && ( ( ccwat * ccwbt ) <= FLT_EPSILON ) )
+  if( ( ( ccwta * ccwtb ) <= FLT_EPSILON ) && ( ( ccwat * ccwbt ) <= FLT_EPSILON ) )
+  {
+    //now calculate the lambda:
+    Point2d lambda1 = ( q->getA() - p->getA() ) / ( p->getB() - p->getA() );
+
+    //calculate the point from lambda:
+    intersection = new Point2d ( ((p->getA().getX() * p->getB().getY() - p->getA().getY() *  p->getB().getX())*(q->getA().getX() - q->getB().getX())-(p->getA().getX() - p->getB().getX()) * (q->getA().getX() * q->getB().getY() - q->getA().getY() * q->getB().getX()) / (p->getA().getX() - p->getB().getX())*(q->getA().getY() - q->getB().getY()) - (p->getA().getY() - p->getB().getY()) * (q->getA().getX() - q->getB().getX()))
+      ,
+        ((p->getA().getX() * p->getB().getY() - p->getA().getY() * p->getB().getX()) * (q->getA().getY() - q->getB().getY()) - (p->getA().getY() - p->getB().getY()) * (q->getA().getX() * q->getB().getY() - q->getA().getY() * q->getB().getX()) / (p->getA().getX() - p->getB().getX()) * (q->getA().getY() - q->getB().getY()) - (p->getA().getY() - p->getB().getY()) * (q->getA().getX() - q->getB().getX()))
+    );
+
     return true;
+  }
+
   return false;
 }
