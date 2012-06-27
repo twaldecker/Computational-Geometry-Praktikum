076b0a0dae2631c314c1db3aa0bc9d161faa3c8d
Stefan Giggenbach <stefan.giggenbach@hm.edu>
2012-06-27 14:13:07 +0200

    supersheet

--- 4/main.m
+++ 4/main.m
@@ -1,2 +1,12 @@
 %% Praktikum 4 Linear Programming
 
+close all;
+
+% read data from file
+fp = fopen('testpolygon.txt', 'r+');
+P = fscanf( fp, '%f %f', [2 inf] );
+P = P';
+fclose( fp );
+
+% draw polygon
+figure, fill( P(:, 1), P(:,2), 'r' );
