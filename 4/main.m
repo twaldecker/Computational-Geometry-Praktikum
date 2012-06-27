df3943d555a2b4885dbcfdaa8fd9a34e1954d897
Thomas Waldecker <thomas.waldecker@googlemail.com>
2012-06-27 15:19:06 +0200

    ccw

--- 4/main.m
+++ 4/main.m
@@ -3,10 +3,19 @@
 close all;
 
 % read data from file
-fp = fopen('testpolygon.txt', 'r+');
+fp = fopen('polygon.txt', 'r+');
 P = fscanf( fp, '%f %f', [2 inf] );
 P = P';
 fclose( fp );
 
 % draw polygon
 figure, fill( P(:, 1), P(:,2), 'r' );
+
+% calculate orientation of polygon
+
+c = P(1:3,1:2);
+c = horzcat(c, [1;1;1]);
+ccwv = det(c);
+
+
+
