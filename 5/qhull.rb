bf8d1ed79b5b764ac56f5b1e471fc83247b64b29
Thomas Waldecker <thomas.waldecker@googlemail.com>
2012-06-28 14:29:27 +0200

    not working

--- 5/qhull.rb
+++ 5/qhull.rb
@@ -12,20 +12,35 @@ print "Praktikum qhull\n"
 # z.B. Erzeugen von 15 Punkten im 8 Dimensionalen Raum:
 # rbox 15 D8
 
-command = 'rbox %d D%d | qhull'
-dim_range = 2..3
-point_range = 1e3..1e6
+dim_range = 2..8
+point_range = 1e3..1e5
+
+dim_point_range = Array.new()
+
+dim_point_range[2] = 10..1e5
+dim_point_range[3] = 10..1e5
+dim_point_range[4] = 10..1e5
+dim_point_range[5] = 10..1e4
+dim_point_range[6] = 10..1e3
+dim_point_range[7] = 10..1e3
+dim_point_range[8] = 10..1e2
+
+p dim_point_range
+
+colors = ['#e0f0f0', '#c0e0f0', '#a0c0e0', '#80a0e0', '#4080e0', '#4040a0', '#0020c0']
 
 f = File.open('measurement.dat', 'w')
 
 for dim in dim_range
 
-    points = point_range.begin
+    points = dim_point_range[dim].begin
+    
+    f.puts "# #{dim}D (index #{dim-2})\n# X Y\n"
 
-    while points <= point_range.end
+    while points <= dim_point_range[dim].end
 
-        print "calculating convex hull of " + points.to_s + " points in " + dim.to_s + "-D\n"
-        output = IO.popen sprintf(command, points, dim)
+        print "calculating convex hull of #{points} points in #{dim}-D\n"
+        output = IO.popen "rbox #{points} D#{dim} | qhull"
 
         m = /CPU seconds to compute hull \(after input\): (.+)/.match output.read
         
@@ -34,9 +49,29 @@ for dim in dim_range
         points *= 10
     
     end
-    f.puts "\n"
+    f.puts "\n\n"
 end
 
 f.close
 
-system "gnuplot -p -e 'set logscale x; plot \"measurement.dat\"'"
+# Jetzt plotten wir die Ergebnisse mit gnuplot.
+
+a = <<`GNUPLOT`
+gnuplot -p -e "set style line 1 lc rgb '#{colors[0]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 2 lc rgb '#{colors[1]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 3 lc rgb '#{colors[2]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 4 lc rgb '#{colors[3]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 5 lc rgb '#{colors[4]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 6 lc rgb '#{colors[5]}' lt 1 lw 2 pt 7 ps 1.5;
+set style line 7 lc rgb '#{colors[6]}' lt 1 lw 2 pt 7 ps 1.5;
+set logscale x;
+set logscale y;
+set xrange 1:1e5;
+plot 'measurement.dat' index 0 with linespoints ls 1 title '2D', 
+'' index 1 with linespoints ls 2 title '3D',
+'' index 2 with linespoints ls 3 title '4D',
+'' index 3 with linespoints ls 4 title '5D',
+'' index 4 with linespoints ls 5 title '6D',
+'' index 5 with linespoints ls 6 title '7D',
+'' index 6 with linespoints ls 7 title '8D';"
+GNUPLOT
