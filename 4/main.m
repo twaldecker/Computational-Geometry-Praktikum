fa0718bd6e0f0f393f691067eb1fa9ade059b7e2
Stefan Giggenbach <stefan.giggenbach@hm.edu>
2012-06-29 11:38:34 +0200

    p4 Berechnung Normaleneinheitsvektoren

--- 4/main.m
+++ 4/main.m
@@ -2,20 +2,23 @@
 
 close all;
 
-% read data from file
-fp = fopen('polygon.txt', 'r+');
+% Daten einlesen
+fp = fopen('testpolygon.txt', 'r+');
 P = fscanf( fp, '%f %f', [2 inf] );
-P = P';
 fclose( fp );
 
-% draw polygon
-figure, fill( P(:, 1), P(:,2), 'r' );
+% Polygon zeichnen
+figure, fill( P( 1, : ), P( 2, : ), 'r' );
 
-% calculate orientation of polygon
-
-c = P(1:3,1:2);
+% Orientierung des Polygons berechnen
+c = P(1:2,1:3)';
 c = horzcat(c, [1;1;1]);
-ccwv = det(c);
+ccw = det(c);
 
-
+% Berechnung der Normaleneinheitsvektoren
+N = zeros( 2, size( P, 2 ) );
+for i = 1:( size( P, 2 ) - 1 )
+    N( :, i ) = vec1norm( P( :, i ), P( :, i+1 ), ccw );
+end
+N( :, size( P, 2) ) = vec1norm( P( :, end ), P( :, 1 ), ccw );
 
